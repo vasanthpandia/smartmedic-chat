3d62b1f1148ed2d3fd1e0b8f8ba21709d785b7b8
Vasanth <itsmevasanth@hotmail.com>
2017-01-01 21:14:03 +0530

    Login started

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Hapi = require('hapi');
+const rp = require('request-promise');
 
 const server = new Hapi.Server();
 server.connection({
@@ -10,12 +11,33 @@ const io = require('socket.io')(server.listener);
 
 server.route({
   method: 'GET',
-  path:'/hello',
+  path:'/status',
   handler: function (request, reply) {
       console.log(server.listener);
-      return reply('hello world');
+      return reply(`You've reached SmartMedic`);
   }
 });
+
+server.route({
+  method: 'POST',
+  path: '/login',
+  handler: function(request, reply) {
+    const params = JSON.parse(request.payload);
+
+    rp({
+      url: process.env.LOGIN_URL,
+      method: `POST`,
+      form: {
+        email: `${params['email']}`,
+        password: `${params['password']}`
+      }
+    }).then(response => {
+      reply(response);
+    }).catch(error => {
+      reply(error);
+    })
+  }
+})
 
 server.start((err) => {
   if (err) {
